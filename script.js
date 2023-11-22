1b13aab0933f6f1d7791111531c3baaf735d4e14
JoaoEmerson0 <113241260+JoaoEmerson0@users.noreply.github.com>
2023-11-22 06:39:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+let perguntaAtual = 1;
+let acertos = 0;
+let erros = 0;
+let tempoTotal = 0;
+let tempoMedio = 0;
+let contadorTempo;
+
+function iniciarQuiz() {
+    document.getElementById('tela-inicial').style.display = 'none';
+    document.getElementById(`pergunta${perguntaAtual}`).style.display = 'block';
+    iniciarContadorTempo();
+}
+
+function iniciarContadorTempo() {
+    contadorTempo = setInterval(() => {
+        tempoTotal++;
+    }, 1000);
+}
+
+function mostrarProximaPergunta(perguntaAtualID) {
+    const opcoes = document.getElementsByName(`pergunta${perguntaAtual}`);
+    let respostaSelecionada = null;
+
+    opcoes.forEach(opcao => {
+        if (opcao.checked) {
+            respostaSelecionada = opcao.id;
+        }
+    });
+
+    if (respostaSelecionada) {
+        const label = document.querySelector(`label[for=${respostaSelecionada}]`);
+        label.classList.add('selecionada');
+    }
+
+    const perguntaAtualElement = document.getElementById(perguntaAtualID);
+    perguntaAtualElement.classList.add('animated', 'fade-out');
+
+    setTimeout(() => {
+        perguntaAtualElement.style.display = 'none';
+        perguntaAtualElement.classList.remove('animated', 'fade-out');
+
+        perguntaAtual++;
+        if (perguntaAtual <= 5) {
+            const proximaPerguntaElement = document.getElementById(`pergunta${perguntaAtual}`);
+            proximaPerguntaElement.style.display = 'block';
+            proximaPerguntaElement.classList.add('animated', 'fade-in');
+            reiniciarContadorTempo();
+        } else {
+            pararContadorTempo();
+            mostrarResultado();
+        }
+    }, 1000); // 1000 milissegundos = 1 segundo, ajuste conforme necessário
+}
+
+
+function reiniciarContadorTempo() {
+    clearInterval(contadorTempo);
+    iniciarContadorTempo();
+}
+
+function pararContadorTempo() {
+    clearInterval(contadorTempo);
+}
+
+function mostrarResultado() {
+    for (let i = 1; i <= 5; i++) {
+        const perguntaElement = document.getElementById(`pergunta${i}`);
+        perguntaElement.style.display = 'none';
+    }
+
+    const resultadoElement = document.getElementById('resultado');
+    resultadoElement.style.display = 'block';
+
+    const acertosElement = document.getElementById('acertos');
+    acertosElement.textContent = acertos;
+
+    const errosElement = document.getElementById('erros');
+    errosElement.textContent = erros;
+
+    // Calcular tempo médio por resposta
+    tempoMedio = tempoTotal / 5;
+
+    const tempoMedioElement = document.getElementById('tempoMedio');
+    tempoMedioElement.textContent = tempoMedio.toFixed(2) + ' segundos';
+}
+
+
+
+
+function recomecarQuizz() {
+    // Redirecionar para a mesma página
+    window.location.href = window.location.href;
+}
